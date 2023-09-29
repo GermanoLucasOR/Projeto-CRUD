7a52918287b0d3f746b9b2aeb897c3a3f5a149e1
Germano Lucas <127906965+GermanoLucasOR@users.noreply.github.com>
2023-09-29 20:35:49 -0300

    Update Cela.h

--- Cela.h
+++ Cela.h
@@ -1,20 +1,34 @@
+#include "Pessoa.h"
+#include "Prisioneiro.h"
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Prisioneiro.h"
-#include "Pessoa.h"
 #pragma once
 
 using namespace std;
 
-class Cela{
-  public: 
-    Cela();
-    Prisioneiro* getPrisioneiro(string id);
-    void adicionarPrisioneiro(Pessoa* p);
-    void penaPaga(string i);
+class Cela {
+public:
+  Cela();
+  // void lerPrisioneiro(ifstream& lp);
+  Prisioneiro *getPrisioneiro(string id);
+  void adicionarPrisioneiro(Pessoa *p);
+  int getNumero();
+  int contaPrisioneiros();
+  bool pExisteC(string id, string cpf);
+  bool pExiste(string id);
+  bool pConfereCPF(string cpf);
+  void setNumero(int n);
+  void setVectorP(vector<Pessoa *> p);
+  void exibirTodosP();
+  void alterarPrisioneiro(string cpf);
+  void removerPrisioneiro(string cpf);
+  void confirmarRemocaoP(string id);
+  void salvarPrisioneiro(ofstream &listadeprisioneiros);
+  // void novoPrisioneiro();
 
-  private:
-    vector<Pessoa*> prisioneiro;
+private:
+  vector<Pessoa *> prisioneiro;
+  int numero;
 };
-
