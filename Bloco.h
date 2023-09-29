19301cd26e9794b6442d015b19e8156fa6199a26
Germano Lucas <127906965+GermanoLucasOR@users.noreply.github.com>
2023-09-29 20:34:34 -0300

    Update Bloco.h

--- Bloco.h
+++ Bloco.h
@@ -1,24 +1,42 @@
+#include "Cela.h"
+#include "Funcionario.h"
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Cela.h"
-#include "Funcionario.h"
 #pragma once
 
 using namespace std;
 
 class Bloco {
-  public:
-    Bloco();
-    Cela getCela(int i);
-    string getGenero();
-    Funcionario getFuncionario(string id);
-    void adicionarCela(Cela c);
-    void setGenero(string g);
-    void adicionarFuncionario(Pessoa *f);
+public:
+  Bloco();
+  Cela* getCelaN(int n); 
+  void lerFuncionario();
+  void lerPrisioneiros();
+  void lerCela(); 
+  Cela* getCela(string id);
+  Funcionario *getFuncionario(string id);
+  int contaFuncionarios();
+  int contaCelas();
+  int contaP();
+  int contaTudo();
+  bool pExisteB(Pessoa* p);
+  bool pExisteID(string id);
+  bool fExiste(string id);  
+  void exibirTodosF();
+  void fConfereCPF(Pessoa *f);
+  bool pConfereCpfBloco(Pessoa *p);
+  void adicionarCela(Cela *c);
+  void adicionarFuncionario(Pessoa *f);
+  void alterarFuncionario(string cpf);
+  void confirmarRemocaoF(string id);
+  void removerFuncionario(string id);
+  void salvarFuncionario();
+  void salvarPrisioneiro();
+  void salvarCela();
+  void exibirTodosP();
 
-  private:
-    vector<Cela> cela;
-    string genero;
-    vector<Pessoa*> funcionario;
+private:
+  vector<Cela*> cela;
+  vector<Pessoa*> funcionario;
 };
